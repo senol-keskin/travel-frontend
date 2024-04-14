acf6e49c3f08f57148c9799a32a16bcc66edc3d2
Senol Keskin <senolk@lidyateknoloji.com>
2024-04-14 12:29:24 +0300

    fix; tests

---
+++ src/components/button.test.tsx
@@ -0,0 +1,11 @@
+import "@testing-library/jest-dom"
+
+import { render, screen } from "@/test-util"
+import Button from "./button"
+
+describe("Button Component", () => {
+  it("should have link text", () => {
+    render(<Button />)
+    expect(screen.getByRole("button")).toHaveTextContent("click")
+  })
+})
